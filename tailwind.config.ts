818bef7f8ed1fe09914c720acec57447f08b0955
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-11-08 09:53:06 +0100

    style: Definuj třídu container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,23 @@ export default {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          sm: '1rem',
+          md: '1.5rem',
+          lg: '2rem',
+          xl: '3rem',
+        },
+        screens: {
+          sm: '100%',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
