abd69188f14ea14da8d0641e13edb858625afcd8
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-11-08 18:10:56 +0100

    feat: Vytvoř základ MenuButton komponenty

---
+++ components/layout/Header/MenuButton.tsx
@@ -0,0 +1,11 @@
+import { IoMdMenu } from 'react-icons/io';
+
+const MenuButton = () => {
+  return (
+    <button className='h-full py-3'>
+      <IoMdMenu className='h-full w-full' />
+    </button>
+  );
+};
+
+export default MenuButton;
