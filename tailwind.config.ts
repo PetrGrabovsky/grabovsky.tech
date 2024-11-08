0f5d5195c654eb1b843c579e4813bdec740f4583
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-11-08 15:19:07 +0100

    style: Přidej základní definice do tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,22 +2,33 @@ import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      colors: {
+        'background-body': '#141414',
+        'background-primary': '#171717',
+        'background-secondary': '#212121',
+        'text-primary': '#cccccc',
+        'accent-primary': '#404040',
+        'brand-primary': '#00cc33',
+        'brand-secondary': '#663399',
+      },
+      boxShadow: {
+        'shadow-primary': '0 0 10px rgba(102, 51, 153, 0.5)',
+      },
       container: {
         center: true,
         padding: {
+          DEFAULT: '0.5rem',
           sm: '1rem',
           md: '1.5rem',
           lg: '2rem',
           xl: '3rem',
         },
         screens: {
-          sm: '100%',
           md: '768px',
           lg: '1024px',
           xl: '1280px',
